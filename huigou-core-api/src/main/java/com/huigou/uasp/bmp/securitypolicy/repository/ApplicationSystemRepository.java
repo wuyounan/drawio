4d1f3fd6ee79d6a13de5539752b004d2d188d5af
雍欢 <cpjit@qq.com>
2019-12-24 10:15:22 +0800

    多数据库支持

--- huigou-core-api/src/main/java/com/huigou/uasp/bmp/securitypolicy/repository/ApplicationSystemRepository.java
+++ huigou-core-api/src/main/java/com/huigou/uasp/bmp/securitypolicy/repository/ApplicationSystemRepository.java
@@ -7,25 +7,24 @@ import org.springframework.data.repository.query.Param;
 import com.huigou.uasp.log.domain.model.ApplicationSystem;
 
 
-
 public interface ApplicationSystemRepository extends JpaRepository<ApplicationSystem, String> {
-	ApplicationSystem findByCode(String code);
-	
-	@Query(countName="applicationSystem.countByCode",value="select count(*) from sa_applicationsystem t where t.code = ?1 and t.id != ?2",nativeQuery=true)
-	Long countByCodeAndId(String code,String id);
-	
-	@Query(countName="applicationSystem.countByName",value="select count(*) from sa_applicationsystem t where t.code = ?1 and t.id != ?2",nativeQuery=true)
-	Long countByNameAndId(String name,String id);
-	
-	@Query(countName="applicationSystem.countByName",value="select count(*) from sa_applicationsystem t where t.class_prefix = ?1 and t.id != ?2",nativeQuery=true)
-	Long countByClassPrefixAndId(String ClassPrefix,String id);
+    ApplicationSystem findByCode(String code);
 
-	@Query(name = "applicationSystem.findByClassPrefix", nativeQuery = true, value = "select * from SA_ApplicationSystem where :classPrefix like class_prefix || '%'")
-	ApplicationSystem findByClassPrefix(@Param("classPrefix") String classPrefix);
-	
-	Long countByCode(String code);
-	
-	Long countByName(String name);
-	
-	Long countByClassPrefix(String ClassPrefix);
+    @Query(countName = "applicationSystem.countByCode", value = "select count(t) from ApplicationSystem t where t.code=?1 and t.id !=?2")
+    Long countByCodeAndId(String code, String id);
+
+    @Query(countName = "applicationSystem.countByName", value = "select count(t) from ApplicationSystem t where t.code=?1 and t.id !=?2")
+    Long countByNameAndId(String name, String id);
+
+    @Query(countName = "applicationSystem.countByName", value = "select count(t) from ApplicationSystem t where t.classPrefix = ?1 and t.id != ?2")
+    Long countByClassPrefixAndId(String ClassPrefix, String id);
+
+    @Query(name = "applicationSystem.findByClassPrefix", value = "from ApplicationSystem where :classPrefix like concat(classPrefix,'%')")
+    ApplicationSystem findByClassPrefix(@Param("classPrefix") String classPrefix);
+
+    Long countByCode(String code);
+
+    Long countByName(String name);
+
+    Long countByClassPrefix(String ClassPrefix);
 }
